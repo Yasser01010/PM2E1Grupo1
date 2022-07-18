9f0bc86e01a70a32c1542ddc52cd346096f26c5c
YovannyUTH <107007204+yovannyuth@users.noreply.github.com>
2022-07-17 22:48:39 -0600

    cambios

--- app/src/main/java/com/example/pm2e1grupo10/MenuActivity.java
+++ app/src/main/java/com/example/pm2e1grupo10/MenuActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -12,9 +13,17 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+      //  Button agregar = findViewById(R.id.button);
+      //  Button agrega = findViewById(R.id.button2);
     }
     public void Agregar(View view){
         Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+
+    public void Lista(View view){
+        Intent i = new Intent(this,ListaContactoActivity.class);
         startActivity(i);
     }
 }
